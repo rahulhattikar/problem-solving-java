1e71980120c1e607e336e4fb9743613e47280f0e
rahul <rahulhattikar1798@gmail.com>
2026-05-14 11:45:33 +0530

    Added Range sum query problem 303

---
+++ NumArray.java
@@ -0,0 +1,16 @@
+class NumArray{
+
+    int [] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+        for (int i = 1 ; i < nums.length ; i++){
+            nums[i] += nums[i - 1];
+        }  
+    }
+    
+    public int sumRange(int left, int right) {     
+       return left != 0 ? nums[right] - nums[left - 1] : nums[right];
+    }
+}
+
